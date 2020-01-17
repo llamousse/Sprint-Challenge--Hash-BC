6110e175aa9382ed6a731c769f81972ce80edf27
Vicky <vyue013@gmail.com>
2020-01-17 10:38:32 -0800

    ex2 working

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -20,4 +20,22 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
 
+    # hash 
+    # key:starting location / value: destination
+    # 'i'th location in route can be found by checking
+    # the hash table for the 'i-1'th location
+
+    # hash ticket with 
+    # key(t.source) and value(t.destination)
+    for t in tickets:
+        hash_table_insert(hashtable, t.source, t.destination)
+    
+    # get first ticket since ticket source is "NONE"
+    destination = hash_table_retrieve(hashtable, "NONE")
+    i = 0
+    while i < len(route):
+        route[i] = destination
+        i += 1
+        destination = hash_table_retrieve(hashtable, destination)
+    
     return route
