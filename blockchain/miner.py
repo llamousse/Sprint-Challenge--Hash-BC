22cda2cbf033f4caadb9cb4390be1f5da33cf3cd
Vicky <vyue013@gmail.com>
2020-01-17 10:58:33 -0800

    mvp - miner working, wip time

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -24,8 +24,14 @@ def proof_of_work(last_proof):
 
     print("Searching for next proof")
     proof = 0
+    
     #  TODO: Your code here
-
+    lproof = f'{last_proof}'.encode()
+    last_hash = hashlib.sha256(lproof).hexdigest()
+    
+    while valid_proof(last_hash, proof):
+        proof += 1
+ 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
 
@@ -40,7 +46,9 @@ def valid_proof(last_hash, proof):
     """
 
     # TODO: Your code here!
-    pass
+    guess = f'{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    return last_hash[-6:] == guess_hash[:6]
 
 
 if __name__ == '__main__':
